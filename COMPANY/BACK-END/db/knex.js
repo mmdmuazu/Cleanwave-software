55ad9edd9bd651ee8eca175bd502463e6ebb00e6
mmdmuazu <162482343+mmdmuazu@users.noreply.github.com>
2026-01-03 14:01:56 +0100

    initial commit

--- COMPANY/BACK-END/db/knex.js
+++ COMPANY/BACK-END/db/knex.js
@@ -1,1 +1,10 @@
-/
+const knex = require("knex");
+const config = require("../knexfile.js");
+
+// Use the environment variable to determine which config to use (development, production, etc.)
+// Defaults to 'development' if no environment is set
+const environment = process.env.NODE_ENV || "development";
+const configOptions = config[environment];
+
+module.exports = knex(configOptions);
+
